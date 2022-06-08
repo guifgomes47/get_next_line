fc346c6b4bc24a19e0474160f2a4387479558268
guifgomes47 <guifgomes16@gmail.com>
2022-06-08 14:05:14 -0300

    bonus

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guilhfer <guilhfer@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/24 16:47:10 by guifgomes         #+#    #+#             */
+/*   Updated: 2022/06/08 11:06:53 by guilhfer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	count;
+
+	count = 0;
+	while (str[count])
+	{
+		count++;
+	}
+	return (count);
+}
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size <= 0)
+	{
+		return (ft_strlen(src));
+	}
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (ft_strlen(src));
+}
+
+char	*ft_strdup(const char *str)
+{
+	char	*cpy;
+	size_t	size;
+
+	size = ft_strlen(str) + 1;
+	cpy = malloc(sizeof(char) * size);
+	if (cpy == NULL)
+		return (NULL);
+	ft_strlcpy(cpy, str, size);
+	return (cpy);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	str = malloc(sizeof(char) * (s1_len + s2_len) + 1);
+	if (!str)
+		return (NULL);
+	s1_len = 0;
+	while (s1[s1_len])
+	{
+		str[s1_len] = s1[s1_len];
+		s1_len++;
+	}
+	s2_len = 0;
+	while (s2[s2_len])
+	{
+		str[s1_len++] = s2[s2_len++];
+	}
+	str[s1_len] = '\0';
+	free(s1);
+	return (str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < ft_strlen(s) + 1)
+	{
+		if (s[i] == (char)c)
+		{
+			return ((char *)s + i);
+		}
+		i++;
+	}
+	return (NULL);
+}
